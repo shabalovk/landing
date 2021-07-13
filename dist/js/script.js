0594ac2379ae5f67bba32f5bf6031d2017fb68db
kirill-frontend <shabalov@appetite.menu>
2021-07-13 18:35:48 +0600

    desctop is done

---
+++ dist/js/script.js
@@ -0,0 +1,31 @@
+
+window.addEventListener('DOMContentLoaded', () => {
+    const openModalBtn = document.querySelector('.container__btn');
+    const modal = document.querySelector('.modal');
+    const closeModal = document.querySelector('.modal__close');
+    
+    openModalBtn.addEventListener('click', () => {
+        modal.classList.add('active')
+    })
+
+    closeModal.addEventListener('click', () => {
+        modal.classList.remove('active')
+    })
+
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            modal.classList.remove('active')
+        }
+    })
+
+    $('.container__carousel').slick({
+        centerMode: true,
+        slidesToShow: 1,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        draggable: false,
+        pauseOnFocus: false,
+        pauseOnHover: false
+    });
+})
